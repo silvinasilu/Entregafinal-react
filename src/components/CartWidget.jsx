118b71d8f66afe3c9f5ade90ae872902dff3eb10
Silvina Lotitto <silvilotitto@gmail.com>
2023-09-18 21:00:15 -0300

    agrega itemCount

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,8 +1,10 @@
-import { IconButton, Divider, VStack, Image, Center, Drawer, Flex, Text, DrawerBody, DrawerHeader, DrawerFooter, Stack, DrawerOverlay, DrawerContent, Button } from "@chakra-ui/react"
+import { IconButton, Box, VStack, Image, Center, Drawer, Flex, Text, DrawerBody, DrawerHeader, DrawerFooter, Stack, DrawerOverlay, DrawerContent, Button } from "@chakra-ui/react"
 import { Delete } from "@mui/icons-material";
 import { useContext, useState } from "react";
 import { CartContext } from "./CartContext";
 import Modal from "./Modal";
+import ItemCount from "./ItemCount";
+
 
 
 export default function CartWidget({ isOpen, onOpen, onClose }) {
@@ -19,38 +21,44 @@ export default function CartWidget({ isOpen, onOpen, onClose }) {
                     {
                         carrito ?
                             carrito.map(producto =>
-                                <Flex
-                                    borderBottomWidth={"2px"}
-                                    borderBottomColor={"pink.700"}
-                                    alignItems="center"
-                                    justifyContent="space-between"
-                                    rounded bg="pink.200"
-                                    color="pink.900"
-                                    key={producto.id}
-                                >
-                                    <Center p="2">
-                                        <Image
-                                            boxSize='100px'
-                                            objectFit='cover'
-                                            src={producto.foto}
-                                            alt='Dan Abramov'
-                                        ></Image>
-                                    </Center>
-                                    <VStack>
-                                        <Text fontWeight="700" w="100%">{producto.nombre}</Text>
-                                        <Text>${producto.precio}</Text>
-                                    </VStack>
-                                    <Center>
-                                        <IconButton
-                                            border="none"
-                                            _hover={{ border: "none", bg: "pink.200" }}
-                                            bg="pink.200"
-                                            color="red.500"
-                                            icon={<Delete size="xl" />}
-                                            onClick={() => sacarDelCarrito(producto.id)}
-                                        />
-                                    </Center>
-                                </Flex>) :
+                                <Box>
+                                    <Flex
+                                        borderBottomWidth={"2px"}
+                                        borderBottomColor={"pink.700"}
+                                        alignItems="center"
+                                        justifyContent="space-between"
+                                        rounded bg="pink.200"
+                                        color="pink.900"
+                                        key={producto.id}
+                                    >
+                                        <Center p="2">
+                                            <Image
+                                                boxSize='100px'
+                                                objectFit='cover'
+                                                src={producto.foto}
+                                                alt='Dan Abramov'
+                                            ></Image>
+                                        </Center>
+                                        <VStack>
+                                            <Text fontWeight="700" w="100%">{producto.nombre}</Text>
+                                            <Text>${producto.precio}</Text>
+                                            <Text>{producto.stock}</Text>
+                                        </VStack>
+                                        <Center>
+                                            <IconButton
+                                                border="none"
+                                                _hover={{ border: "none", bg: "pink.200" }}
+                                                bg="pink.200"
+                                                color="red.500"
+                                                icon={<Delete size="xl" />}
+                                                onClick={() => sacarDelCarrito(producto.id)}
+                                            />
+                                        </Center>
+                                    </Flex>
+                                    <Flex justifyContent={"center"} p="2" bg="pink.100">
+                                        <ItemCount />
+                                    </Flex>
+                                </Box>) :
                             <Center p="5%">
                                 <Text color="black">No tenés productos cargados...</Text>
                             </Center>
@@ -61,9 +69,9 @@ export default function CartWidget({ isOpen, onOpen, onClose }) {
                 <Button
                     bg="pink.100"
                     color="pink.500"
-                    onClick={() => { 
-                        vaciarCarrito(); 
-                        setModalAbierto(true) 
+                    onClick={() => {
+                        vaciarCarrito();
+                        setModalAbierto(true)
                     }}
                 >Comprar</Button>
             </DrawerFooter>
--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ButtonGroup, Button } from '@chakra-ui/react'
 
 function ItemCount() {
+  const [cantidadAComprar, setCantidadAComprar]=useState(1);
+
+  function sumarProducto() {
+    setCantidadAComprar(cantidadAComprar + 1)
+  }
+  function restarProducto() {
+    setCantidadAComprar(cantidadAComprar ? cantidadAComprar - 1 : cantidadAComprar)
+  }
   return (
-    <div>ItemCount</div>
+    <ButtonGroup variant='outline' spacing='6'>
+      <Button bg="pink.600" color="white" onClick={sumarProducto}>+</Button>
+      <Button bg="white" color="pink.600" onClick={restarProducto}>-</Button>
+      <Button bg="pink.100" color="pink.600" borderColor= "pink.100">{cantidadAComprar}</Button>
+    </ButtonGroup>
   )
 }
 
